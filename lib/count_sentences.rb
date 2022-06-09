0b1478996983ba7dd79240e6932226c86249d95f
Martin <mkmartinoes@gmail.com>
2022-06-09 22:57:29 +0300

    Complitted Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    word = self
+    word.split(/\?|\.|!/).reject(&:empty?).length
   end
 end
+
+# "Hello? World!! Hell...".count_sentences
